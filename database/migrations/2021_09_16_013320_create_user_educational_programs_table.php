6594800725ad90b7dcbedac390a8d5dc546fb321
Andrew <andrewsh190@gmail.com>
2021-09-24 11:31:05 -0400

    feat: add migrations; issue #1

---
+++ database/migrations/2021_09_16_013320_create_user_educational_programs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserEducationProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_educational_programs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->constrained("users");
+            $table->foreignId("educational_program_id")->constrained("educational_programs");
+            $table->date("save_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_educational_programs');
+    }
+}
