ae7c8d4437c7c8170752d0c4d1c29a3c57187645
Nadish-Madadi <75193860+Nadish-Madadi@users.noreply.github.com>
2021-09-26 01:22:48 -0400

    update: posts relationship added; issue #4

--- app/Models/Crew.php
+++ app/Models/Crew.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Crew extends Model
 {
@@ -20,5 +21,8 @@ class Crew extends Model
 
     public function institutions(): HasOne {
         return $this->hasOne(Institutions::class);
+    }
+    public function posts(): HasManyThrough {
+        return $this->hasManyThrough(Post::class, 'author_post');
     }
 }
