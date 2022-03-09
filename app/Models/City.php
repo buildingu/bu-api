1465bbac02217b8643913597f80d67c37f72a10a
Andrew <andrewsh190@gmail.com>
2022-03-08 20:45:11 -0500

    fix: declare return type

--- app/Models/City.php
+++ app/Models/City.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class City extends Model
 {
@@ -17,11 +19,11 @@ class City extends Model
         'state_id'
     ];
 
-    public function state() {
+    public function state(): BelongsTo {
         return $this->belongsTo(State::class);
     }
 
-    public function users() {
+    public function users(): BelongsToMany {
         return $this->belongsToMany(User::class);
     }
 
--- app/Models/State.php
+++ app/Models/State.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class State extends Model
 {
@@ -14,11 +16,11 @@ class State extends Model
         'name',
     ];
 
-    public function country() {
+    public function country(): BelongsTo {
         return $this->belongsTo(Country::class);
     }
 
-    public function cities() {
+    public function cities(): HasMany {
         return $this->hasMany(City::class);
     }
 
